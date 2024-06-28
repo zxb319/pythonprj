20f23e80704d5a74ccc6a586cc0386422bd93da9
zxb <zxb319@qq.com>
2024-06-28 17:27:52 +0800

    thinkpad提交

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import uuid
 
 
 class DevConfig:
-    JWT_KEY=uuid.uuid4().hex
-    JWT_EXP_SECS=3600*1
+    JWT_KEY = uuid.uuid4().hex
+    JWT_EXP_SECS = 3600 * 1
 
 
-Config=DevConfig
+Config = DevConfig
